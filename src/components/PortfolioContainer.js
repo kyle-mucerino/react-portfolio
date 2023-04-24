8feb70551d1a7c99e27b7d6fd9950a54f26e2c5c
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-24 11:19:40 -0700

    NavTabs.js created as well as pages folder

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
