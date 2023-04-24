872da8c64ecc1a4eb5b41b498ca44652c158509b
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-24 11:27:21 -0700

    PortfolioContainer.js created. About me page also

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -5,3 +5,28 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        This is where I will feature a section describing myself
+      </p>
+    </div>
+  )
+}
