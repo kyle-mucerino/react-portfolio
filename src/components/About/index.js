db9a8f051186c06397679d46427a138b2d81c8f1
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-27 10:01:32 -0700

    styling Header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function About() {
   return (
-    <section className="my-5">
+    <section>
       <div className="my-2">
-        <i class="fa-sharp fa-solid fa-user" style={{ fontSize: "96px" }}></i>
+        <h1 className='fs-1 badge bg-primary text-wrap' style={{width: "12rem"}}>About Me</h1>
       </div>
       <p>
         This is where I will have a section of info about myself 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1 className='--bs-primary'>Home Page</h1>
+      <h1 className='fs-1 badge bg-primary text-wrap' style={{width: "12rem"}}>Home Page</h1>
       <p>
         Welcome to Kyle Mucerino's portfolio! Please navigate around and find information about myself, ways to get in contact, and previous projects I have worked on. Thank you for visiting!
       </p>
