5d774762bbf751a379af6914b36c05f8b9d36569
kyle-mucerino <kyle.mucerino@gmail.com>
2023-08-17 10:11:55 -0700

    media query

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import babeProfile from "../../assets/BabeProfile2.png";
+import DKCalcSS from '../../assets/DKCalcSS.png';
 import "./portfolio.css";
 
 export default function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
+      <h1><i>Projects</i></h1>
       <div className="project-card-container">
         <div className="project-card">
           <img
@@ -31,6 +32,34 @@ export default function Projects() {
               rel="noopener noreferrer"
               className="btn view-project-btn"
             >
+              View App
+            </a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img
+            src={DKCalcSS}
+            alt="mortgagte calc screenshot"
+            className="project-image"
+            />
+          <div className="project-info">
+            <h2>Mortgage Calculator</h2>
+            <p>
+              An API integrated Mortgage Calculator{" "}
+              <a
+                href="https://mortgage-calculator-2d0e477c8d36.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                website
+              </a>
+            </p>
+            <a
+              href="https://mortgage-calculator-2d0e477c8d36.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn view-project-btn"
+              >
               View App
             </a>
           </div>
