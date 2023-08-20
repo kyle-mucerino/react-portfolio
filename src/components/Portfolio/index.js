ebaa1d750c560cea0a558aa83bd5056fbd3096b4
kyle-mucerino <kyle.mucerino@gmail.com>
2023-08-20 13:27:28 -0700

    rw added to projects page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import babeProfile from "../../assets/BabeProfile2.png";
 import DKCalcSS from '../../assets/DKCalcSS.png';
+import RWSS from '../../assets/RWSS.png';
 import "./portfolio.css";
 
 export default function Projects() {
   return (
     <div>
-      <h1><i>Projects</i></h1>
+      <h1>
+        <i>Projects</i>
+      </h1>
       <div className="project-card-container">
         <div className="project-card">
           <img
@@ -41,25 +44,39 @@ export default function Projects() {
             src={DKCalcSS}
             alt="mortgagte calc screenshot"
             className="project-image"
-            />
+          />
           <div className="project-info">
             <h2>Mortgage Calculator</h2>
             <p>
               An API integrated Mortgage Calculator{" "}
-              <a
-                href="https://mortgage-calculator-2d0e477c8d36.herokuapp.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                website
-              </a>
             </p>
             <a
               href="https://mortgage-calculator-2d0e477c8d36.herokuapp.com/"
               target="_blank"
               rel="noopener noreferrer"
               className="btn view-project-btn"
-              >
+            >
+              View App
+            </a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img
+            src={RWSS}
+            alt="random workout screenshot"
+            className="project-image"
+          />
+          <div className="project-info">
+            <h2>Find a Workout</h2>
+            <p>
+              An API integrated random workout generator{" "}
+            </p>
+            <a
+              href="https://dashboard.heroku.com/apps/random-workout"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn view-project-btn"
+            >
               View App
             </a>
           </div>
